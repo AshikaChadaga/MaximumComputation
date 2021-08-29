565e0fca1e5379b8b5b9254b693d62b228b3b4a8
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-29 11:08:25 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/maximumcomputation/MaximumComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.maximumcomputation;
+
+public class MaximumComputation {
+
+	public static void main(String[] args) {
+		System.out.println("----- Welcome To Computing Maximum Of Three Numbers Using Java Generics ------");
+	}
+
+}
